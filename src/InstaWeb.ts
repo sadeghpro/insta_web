87cd079341c0317036d5a4d691a69ed33e1841a2
sadeghpro <sadeghpro91@gmail.com>
2023-12-20 17:30:55 +0330

    Add get page data by username

--- src/InstaWeb.ts
+++ src/InstaWeb.ts
@@ -1,32 +1,49 @@
-import axios, { Axios, CreateAxiosDefaults } from "axios";
+import axios, { Axios, AxiosHeaders, CreateAxiosDefaults } from "axios";
 import qs from "qs";
 
 
 export default class InstaWeb {
 
     axios: Axios;
+    headers: AxiosHeaders;
 
-    constructor(options: CreateAxiosDefaults) {
+    constructor(options: CreateAxiosDefaults = {}, headers: AxiosHeaders = new AxiosHeaders()) {
         this.axios = axios.create({
             ...options,
         });
+        headers['X-IG-App-ID'] = "936619743392459";
+        this.headers = headers;
     }
+
+    
 
 
     async getPostByShortCode(code: string) {
         let data = qs.stringify({
-          'variables': `{"shortcode": "${code}","fetch_comment_count":40,"fetch_related_profile_media_count":3,"parent_comment_count":24,"child_comment_count":3,"fetch_like_count":10,"fetch_tagged_user_count":null,"fetch_preview_comment_count":2,"has_threaded_comments":true,"hoisted_comment_id":null,"hoisted_reply_id":null}`,
-          'doc_id': '10015901848480474' 
+            'variables': `{"shortcode": "${code}","fetch_comment_count":40,"fetch_related_profile_media_count":3,"parent_comment_count":24,"child_comment_count":3,"fetch_like_count":10,"fetch_tagged_user_count":null,"fetch_preview_comment_count":2,"has_threaded_comments":true,"hoisted_comment_id":null,"hoisted_reply_id":null}`,
+            'doc_id': '10015901848480474'
         });
-        let config = {
-          method: 'post',
-          url: 'https://www.instagram.com/api/graphql',
-          data,
-        };
-        
-        
-        const result = await this.axios.request(config)
+
+        const result = await this.axios.request({
+            method: 'post',
+            url: 'https://www.instagram.com/api/graphql',
+            data,
+            headers: this.headers
+        })
         return result.data.data.xdt_shortcode_media
-        
+
+    }
+
+
+    async getPage(username: string, postCount: number = 12) {
+        if (!this.headers.Cookie) {
+            return new Error('You must login before accessing this page');
+        }
+        const result = await this.axios.request({
+            method: 'get',
+            url: `https://www.instagram.com/api/v1/feed/user/${username}/username/?count=${postCount}`,
+            headers: this.headers,
+        })
+        return result.data
     }
 }
--- test.ts
+++ test.ts
@@ -1,7 +1,10 @@
+import { AxiosHeaders } from "axios";
 import InstaWeb from "./src/InstaWeb"
 
 (async () => {
-    const instaWeb = new InstaWeb({});
-    console.log(await instaWeb.getPostByShortCode('B1OrN1rhPjx40xYoIzh1NTmHZKT3vVM2Iba7UM0'));
-    console.log(await instaWeb.getPostByShortCode('C0QnN_FtV1k'));
+    const headers = new AxiosHeaders();
+    headers.set("Cookie", '');// until create login function we must set cookie before send requests that need authorization
+    const instaWeb = new InstaWeb(undefined, headers);
+    console.log(await instaWeb.getPage('sadeghpro'));
+    
 })()
